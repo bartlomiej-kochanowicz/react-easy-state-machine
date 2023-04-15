a3ca36d2fab3284b8ad9287ef55ca6754891c6e1
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-04-15 13:18:13 +0200

    config build

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
-const packageJson = require("./package.json");
+import pkg from './package.json' assert { type: 'json' };
+
+const globals = {
+  react: 'React',
+};
 
 export default {
-  input: "src/index.ts",
-  output: {
-    sourcemap: false,
-    file: packageJson.main,
-    format: "cjs",
-  },
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      sourcemap: true,
+      exports: 'named',
+      name: 'ReactEasyStateMachine',
+      globals,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  external: Object.keys(globals),
   plugins: [commonjs(), typescript()],
 };
