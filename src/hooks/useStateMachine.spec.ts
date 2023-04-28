0d1b89522a3119e14cd7b2cd768f54c0f47eb733
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-04-28 21:56:49 +0200

    test(usestatemachine.spec.ts): add context tests

--- src/hooks/useStateMachine.spec.ts
+++ src/hooks/useStateMachine.spec.ts
@@ -55,4 +55,84 @@ describe('useStateMachine', () => {
     expect(result.current.compareState('initial')).toBeTruthy();
     expect(result.current.compareState('state1')).toBeFalsy();
   });
+
+  it('should return the context', () => {
+    const { result } = renderHook(() =>
+      useStateMachine<keyof typeof states, keyof typeof actions, { test: string }>(machine),
+    );
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE1, { test: 'test' });
+    });
+
+    expect(result.current.context).toEqual({ test: 'test' });
+  });
+
+  it('should return undefined context', () => {
+    const { result } = renderHook(() =>
+      useStateMachine<keyof typeof states, keyof typeof actions, { test: string }>(machine),
+    );
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE1);
+    });
+
+    expect(result.current.context).toBeUndefined();
+  });
+
+  it('should clean context', () => {
+    const { result } = renderHook(() =>
+      useStateMachine<keyof typeof states, keyof typeof actions, { test: string }>(machine),
+    );
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE1, { test: 'test' });
+    });
+
+    expect(result.current.context).toEqual({ test: 'test' });
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE2);
+    });
+
+    expect(result.current.context).toBeUndefined();
+  });
+
+  it('should update the context', () => {
+    const { result } = renderHook(() =>
+      useStateMachine<keyof typeof states, keyof typeof actions, { test: string }>(machine),
+    );
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE1, { test: 'test' });
+    });
+
+    expect(result.current.context).toEqual({ test: 'test' });
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE2, { test: 'test2' });
+    });
+
+    expect(result.current.context).toEqual({ test: 'test2' });
+  });
+
+  it('should update the context with function', () => {
+    const { result } = renderHook(() =>
+      useStateMachine<keyof typeof states, keyof typeof actions, { test: string }>(machine),
+    );
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE1, { test: 'test' });
+    });
+
+    expect(result.current.context).toEqual({ test: 'test' });
+
+    act(() => {
+      result.current.updateState(actions.CHANGE_TO_STATE2, (context) => ({
+        test: `${context?.test}2`,
+      }));
+    });
+
+    expect(result.current.context).toEqual({ test: 'test2' });
+  });
 });
